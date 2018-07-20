71f2642ddb0c071993a25dde283e9c7f0f43d223
gongyz <1018017334@qq.com>
2018-07-20 14:39:56 +0800

    新增: 头部导航组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import RouterIndex from "./router/index"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import './view/index.css'
 class App extends Component {
   render() {
     return (
-      <RouterIndex/>
+      <div className="page-warp">
+        <Header></Header>
+        <Footer></Footer>
+      </div>
     )
   }
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import {Menu, Icon} from "antd"
+import {Link} from "react-router-dom"
+import propTypes from 'prop-types'
+import './index.css'
+class Nav extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  static propTypes ={
+    mode : propTypes.string
+  }
+
+  static defaultProps = {
+    mode: "horizontal"
+  }
+  
+  render() {
+    let {mode} = this.props
+    return (
+      <Menu 
+        className="header-nav" 
+        mode={mode}
+        theme="dark"
+      >
+        <Menu.Item>
+        <Link to="/index"><Icon type="home" />首页</Link>
+        </Menu.Item>
+        <Menu.Item>
+        <Link to="/book"><Icon type="book" />教程</Link>
+        </Menu.Item>
+        <Menu.Item>
+        <Link to="/about"><Icon type="info-circle-o" />关于</Link>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default Nav
